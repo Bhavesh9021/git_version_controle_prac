61e25b3188acf9f502d7a31d6e3aff64fde29eee
bhavesh patil <patilbhavesh1182003@gmail.com>
2025-08-08 10:49:36 +0530

    increased the size of window and added label

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import tkinter as tk
 
 root = tk.Tk()
+root.title("first GUI app")
+root.geometry("600x800")
+
+label = tk.Label(root, text="Hello World", font=("Arial", 18))
+label.place(relx=0.5,rely=0.5, anchor="center")
 
 root.mainloop()
